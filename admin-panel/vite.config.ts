8e546fb96070528c8fe9a2b74c5a7582249fb790
Bruno Abe <abe2602@gmail.com>
2026-02-20 11:38:12 -0300

    Update env var name

--- admin-panel/vite.config.ts
+++ admin-panel/vite.config.ts
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  envPrefix: ['VITE_', 'BACKEND_'],
   server: {
     proxy: {
       "/api": {
-        target: process.env.VITE_BACKEND_URL ?? "http://localhost:8080",
+        target: process.env.BACKEND_URL ?? "http://localhost:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
